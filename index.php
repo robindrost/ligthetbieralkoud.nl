4b2722316e3c262f09a511d61d7b6af64bd14a1b
FreekVR <freekvanrijt@gmail.com>
2015-01-16 17:10:23 +0100

    Add marquee

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
-<?php
-  print '<h1>Ligt het bier al koud?</h1>';
-  print '<p><strong>Misschien</strong></p>';
-  print '<h1>Wie moet er halen?</h1>';
-  $people = array('Yaron');
-  $key = array_rand($people);
-  print '<em>' . $people[$key] . '</em>';
+<html>
+  <head>
+    <title>Ligt het bier al koud punt NL</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <?php
+      print '<h1>Ligt het bier al koud?</h1>';
+      print '<p><strong>Misschien</strong></p>';
+      print '<h1>Wie moet er halen?</h1>';
+      $people = array('Yaron');
+      $key = array_rand($people);
+      print '<em class="marquee">' . $people[$key] . '</em>';
+    ?>
+  </body>
+</html>
